350dd3d7e40d3af52342b485a66f281c147af361
RubennVdk <ruben.vandekerckhove@student.ucll.be>
2019-11-20 07:20:24 +0100

    http script minor ch

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 subdomain=$(grep -Po '.{1,}(?=.ruben-vandekerckhove.sb.uclllabs.be)' <<< "$1")
-
+echo "$subdomain"
 completedomain=$(cut -d. -f2- <<< "$1")
 firstfield=$(cut -d. -f1 <<< "$1")
 secondfield=$(cut -d. -f2 <<< "$1")
-echo "$completedomain"
-
 search='^'"$firstfield"'\s'
-echo "$search"
-
 found=$(grep -P "$search" /etc/bind/zones/db."$completedomain" | wc -l)
-echo "found"
+echo $(grep -P "$search" /etc/bind/zones/db."$completedomain" | wc -l)
 
 if [ "$found" -gt 0 ]; then
 	mkdir /var/www/html/"$subdomain"
-	touch /var/www/html/"$subdomain"/index.html
-
+	touch /v	ar/www/html/"$subdomain"/index.html
 	echo "<html><body><article><p>welcome $subdomain</p></article></body></html>" >> /var/www/html/"$subdomain"/index.html;
 
 	sed "s/change/$subdomain/g" /etc/apache2/sites-available/.create_vhost.conf > /etc/apache2/sites-available/"$subdomain".conf
